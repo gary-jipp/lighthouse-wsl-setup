08253bbf0a1915ebe9e171e6ad42e325c968e85c
Gary J <github@accu.ca>
2021-12-03 19:52:19 -0800

    Bump version

--- wsl-setup.ps1
+++ wsl-setup.ps1
@@ -2,7 +2,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 $ErrorActionPreference = 'SilentlyContinue'
-$version = "1.02.0209"
+$version = "1.02.021"
 
 function New-Button {
   param ( $x, $action, $text)
