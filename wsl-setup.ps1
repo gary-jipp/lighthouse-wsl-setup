71138e80a2c8ff5dba7652adb5f9ba6c53e6ee34
Gary J <github@accu.ca>
2021-12-03 18:17:07 -0800

    Format

--- wsl-setup.ps1
+++ wsl-setup.ps1
@@ -2,7 +2,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 $ErrorActionPreference = 'SilentlyContinue'
-$version = "1.02.016"
+$version = "1.02.017"
 
 function New-Button {
   param ( $x, $action, $text)
