4d5c999f23e209c787b3350a491e2f65e6cca796
Gary J <github@accu.ca>
2022-01-16 12:51:14 -0800

    Deploy Shoprcut Target Fix

--- wsl-setup.ps1
+++ wsl-setup.ps1
@@ -2,7 +2,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 $ErrorActionPreference = 'SilentlyContinue'
-$version = "1.02.026"
+$version = "1.02.027"
 
 function New-Button {
   param ( $x, $action, $text)
